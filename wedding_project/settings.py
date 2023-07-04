9186ffaef0289d3255bd0a9fc845e2670ecce061
Fiki-josh <fikioluwamayomikun@gmail.com>
2023-07-04 20:41:37 +0100

    push4 to deploy to render

--- wedding_project/settings.py
+++ wedding_project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^rwa-)v*@1e1hixpgve+tyj(3mhc*is@g(6(69^88mmb_cn5s)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
